ec2d290210898e398ee085ac8a97e6cce7e0654d
Vishesh Tiwari <tvishesh593@gmail.com>
2024-01-09 10:34:39 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,22 @@ const elementsToAnimate = document.querySelectorAll('.h1, .h2, .h3');
 elementsToAnimate.forEach(element => {
   observer.observe(element);
 });
+
+$(function () {
+  var selectedDate;
+  $("#datepicker-container").datepicker({
+    inline: true,
+    showOtherMonths: true,
+    onSelect: function (dateText) {
+      selectedDate = dateText;
+    },
+  });
+
+  $("#goButton").on("click", function () {
+    // Set the input value when the user clicks "GO"
+    if (selectedDate) {
+      $(".inputDate").val(selectedDate);
+      console.log("Selected date: " + selectedDate);
+    }
+  });
+});
